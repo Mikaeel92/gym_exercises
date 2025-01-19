28b4b717f0f5bcc258002ab1af7c0509072a2e1b
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-19 13:44:10 +0330

    last steps

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
@@ -8,6 +10,8 @@ import SimilarExercises from '../components/SimilarExercises';
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -33,13 +37,15 @@ const ExerciseDetail = () => {
     fetchExercisesData();
   }, [id]);
 
-  return (
-    <Box>
-      <Detail exerciseDetail={exerciseDetail}/>
-      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises/>
-    </Box>
-  )
-}
+  if (!exerciseDetail) return <div>No Data</div>;
 
-export default ExerciseDetail
+  return (
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,10 @@ const Home = () => {
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart}/>
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
